e4e97bd2cf72a1525602a17c7b804eda92a62142
mdr <matthew.drummond.ritch@gmail.com>
2024-12-27 12:16:07 -0500

    serializers and viewsets

--- questions/serializers.py
+++ questions/serializers.py
@@ -1,10 +1,48 @@
 from rest_framework import serializers
+from models import Thread, Post, Question, Answer, Tag
 
-class MyModelSerializer(serializers.ModelSerializer):
+class ThreadSerializer(serializers.ModelSerializer):
     class Meta:
-        model = None
+        model = Thread
         fields = '__all__'
 
     def create(self, validated_data):
         # Add custom logic here, e.g., setting default values
-        return None
+        return Thread.objects.create(**validated_data)
+    
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def create(self, validated_data):
+        # Add custom logic here, e.g., setting default values
+        return Post.objects.create(**validated_data)
+    
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+    def create(self, validated_data):
+        # Add custom logic here, e.g., setting default values
+        return Question.objects.create(**validated_data)
+    
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+
+    def create(self, validated_data):
+        # Add custom logic here, e.g., setting default values
+        return Answer.objects.create(**validated_data)
+    
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    def create(self, validated_data):
+        # Add custom logic here, e.g., setting default values
+        return Tag.objects.create(**validated_data)
+    
--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,48 @@
-from django.http import HttpResponse
-from rest_framework import viewsets
-from .serializers import MyModelSerializer
+from rest_framework import permissions, viewsets
+from models import Thread, Post, Question, Answer, Tag
+from serializers import ThreadSerializer, PostSerializer, QuestionSerializer, AnswerSerializer, TagSerializer
+
+class ThreadViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Thread objects to be viewed or edited.
+    """
+    queryset = Thread.objects.all().order_by('-dt')
+    serializer_class = ThreadSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the questions index.")
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Post objects to be viewed or edited.
+    """
+    queryset = Post.objects.all().order_by('-dt')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Question objects to be viewed or edited.
+    """
+    queryset = Question.objects.all().order_by('-post__dt')
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Answer objects to be viewed or edited.
+    """
+    queryset = Answer.objects.all().order_by('-post__dt')
+    serializer_class = AnswerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Tag objects to be viewed or edited.
+    """
+    queryset = Tag.objects.all().order_by('name')
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
