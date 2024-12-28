0416b3ace2053af0a405a8259774a7cd2a12618b
mdr <matthew.drummond.ritch@gmail.com>
2024-12-28 13:29:49 -0500

    initial siwe setup

--- questions/models.py
+++ questions/models.py
@@ -1,11 +1,75 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+from web3 import Web3
+
+
+### siwe
+
+
+def validate_ethereum_address(value):
+    if not Web3.isChecksumAddress(value):
+        raise ValidationError
+
+
+class Nonce(models.Model):
+    value = models.CharField(max_length=24, primary_key=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return self.value
+
+
+class WalletManager(BaseUserManager):
+    def create_user(self, address):
+        """
+        Creates and saves a User with the given eth address
+        """
+        if not address:
+            raise ValueError("Users must have an eth address")
+
+        user = self.model(
+            address=address,
+        )
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, address):
+        """
+        Creates and saves a superuser with the given eth address
+        """
+        u = self.create_user(address, password=address)
+        u.is_admin = True
+        u.save(using=self._db)
+        return u
+
+
+class Wallet(AbstractBaseUser):
+    address = models.CharField(
+        verbose_name="Wallet Address",
+        max_length=42,
+        unique=True,
+        validators=[
+            RegexValidator(regex=r"^0x[a-fA-F0-9]{40}$"),
+            validate_ethereum_address,
+        ],
+    )
+
+    is_admin = models.BooleanField(default=False)
+    objects = WalletManager()
+
+
+### for this app
 
 
 class Thread(models.Model):
     topic = models.CharField(max_length=1000)
     dt = models.DateTimeField()
-    n_replies = models.IntegerField(default = 0)
+    n_replies = models.IntegerField(default=0)
 
     def __str__(self):
         return self.topic
@@ -13,7 +77,7 @@ class Thread(models.Model):
 
 class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
-    thread_index = models.IntegerField(default = 0)
+    thread_index = models.IntegerField(default=0)
     text = models.TextField()
     dt = models.DateTimeField()
 
@@ -28,14 +92,12 @@ class Question(models.Model):
         verbose_name="FactHound Question Address",
         max_length=42,
         unique=True,
-        validators=[RegexValidator(regex=r"^0x[a-fA-F0-9]{40}$")],
+        validators=[
+            RegexValidator(regex=r"^0x[a-fA-F0-9]{40}$"),
+            validate_ethereum_address,
+        ],
     )
-    asker = models.CharField(
-        verbose_name="Wallet Address",
-        max_length=42,
-        unique=True,
-        validators=[RegexValidator(regex=r"^0x[a-fA-F0-9]{40}$")],
-    )
+    asker = models.ForeignKey(Wallet)
     bounty = models.IntegerField()  # units of wei
     status = models.CharField(
         choices=[
@@ -55,12 +117,7 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     answerHash = models.BinaryField()
-    answerer = models.CharField(
-        verbose_name="Wallet Address",
-        max_length=42,
-        unique=True,
-        validators=[RegexValidator(regex=r"^0x[a-fA-F0-9]{40}$")],
-    )
+    answerer = models.ForeignKey(Wallet)
     status = models.CharField(
         choices=[
             ("OP", "Open"),
--- questions/views.py
+++ questions/views.py
@@ -1,48 +1,82 @@
-from rest_framework import permissions, viewsets
-from models import Thread, Post, Question, Answer, Tag
-from serializers import ThreadSerializer, PostSerializer, QuestionSerializer, AnswerSerializer, TagSerializer
+import datetime
+import pytz
+import secrets
 
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from rest_framework import permissions, viewsets
+
+from models import Thread, Post, Question, Answer, Tag, Wallet, Nonce
+from serializers import (
+    ThreadSerializer,
+    PostSerializer,
+    QuestionSerializer,
+    AnswerSerializer,
+    TagSerializer,
+)
+from siwe import siwe_required
+
+
+@require_http_methods(["GET"])
+def nonce(request):
+    now = datetime.now(tz=pytz.UTC)
+
+    for n in Nonce.objects.filter(expiration__lte=datetime.now(tz=pytz.UTC)):
+        n.delete()
+    n = Nonce(value=secrets.token_hex(12), expiration=now + datetime.timedelta(hours=3))
+    n.save()
+
+    return JsonResponse({"nonce": n.value})
+
+
+# TODO disable deleting / updating / etc
+
+
+@siwe_required
 class ThreadViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Thread objects to be viewed or edited.
     """
-    queryset = Thread.objects.all().order_by('-dt')
+
+    queryset = Thread.objects.all().order_by("-dt")
     serializer_class = ThreadSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
+@siwe_required
 class PostViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Post objects to be viewed or edited.
     """
-    queryset = Post.objects.all().order_by('-dt')
+
+    queryset = Post.objects.all().order_by("-dt")
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
+@siwe_required
 class QuestionViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Question objects to be viewed or edited.
     """
-    queryset = Question.objects.all().order_by('-post__dt')
+
+    queryset = Question.objects.all().order_by("-post__dt")
     serializer_class = QuestionSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
+@siwe_required
 class AnswerViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Answer objects to be viewed or edited.
     """
-    queryset = Answer.objects.all().order_by('-post__dt')
-    serializer_class = AnswerSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
 
+    queryset = Answer.objects.all().order_by("-post__dt")
+    serializer_class = AnswerSerializer
+
+
+@siwe_required
 class TagViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Tag objects to be viewed or edited.
     """
-    queryset = Tag.objects.all().order_by('name')
+
+    queryset = Tag.objects.all().order_by("name")
     serializer_class = TagSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
