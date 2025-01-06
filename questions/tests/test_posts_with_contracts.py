cdb7086e7f4510c614872b3dbc2abf1dff9e9336
mdr <matthew.drummond.ritch@gmail.com>
2025-01-06 00:45:24 -0500

    add more TestQuestions with contracts involved

--- questions/tests/test_posts_with_contracts.py
+++ questions/tests/test_posts_with_contracts.py
@@ -53,18 +53,15 @@ class TestQuestions(TestCase):
         abi = question_contract["abi"]
         bytecode = question_contract["bytecode"]["object"]
         questionContract = w3.eth.contract(abi=abi, bytecode=bytecode)
-        questionHash = Web3.solidity_keccak(['address', 'string'], [self.asker, question_dict["text"]])
+        questionHash = Web3.solidity_keccak(
+            ["address", "string"], [self.asker, question_dict["text"]]
+        )
         tx_hash = questionContract.constructor(
             self.owner, self.oracle, self.asker, questionHash
-        ).transact(
-            {
-                "from": self.asker,
-                "value": 1
-            }
-        )
+        ).transact({"from": self.asker, "value": 1})
         # wait for the transaction to be mined
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash, 180)
-        #pack the required info
+        # pack the required info
         question_dict["questionAddress"] = tx_receipt["contractAddress"]
         # post about question
         request = self.factory.post(
@@ -107,92 +104,124 @@ class TestQuestions(TestCase):
             qs = Tag.objects.filter(thread=thread, name=t)
             self.assertEqual(qs.count(), 1)
 
-    # def test_reply_to_thread(self):
-    #     # make thread
-    #     request = self.factory.post(
-    #         "/api/post/",
-    #         question_dict,
-    #     )
-    #     request.META["HTTP_AUTHORIZATION"] = f"Bearer {self.token}"
-    #     response = views.post(request)
-    #     content = json.loads(response.content)
-    #     self.assertEqual(response.status_code, 200)
-    #     thread = Thread.objects.get(pk=content["thread"])
-    #     # post again
-    #     post_dict = {"thread": thread.pk, "text": "follow up post"}
-    #     request = self.factory.post(
-    #         "/api/post/",
-    #         post_dict,
-    #     )
-    #     request.META["HTTP_AUTHORIZATION"] = f"Bearer {self.token2}"
-    #     response = views.question(request)
-    #     content = json.loads(response.content)
-    #     # make sure post was created
-    #     post = Post.objects.get(text=post_dict["text"], thread=thread)
-    #     postpk = Post.objects.get(pk=content["post"])
-    #     self.assertEqual(post, postpk)
-    #     self.assertEqual(post.text, post_dict["text"])
-    #     self.assertEqual(self.user2, post.poster)
-    #     # make sure question was created
-    #     question = Question.objects.get(
-    #         post__text=post_dict["text"], post__thread=thread
-    #     )
-    #     questionpk = Question.objects.get(pk=content["question"])
-    #     self.assertEqual(question, questionpk)
-    #     self.assertEqual(question.post.text, post_dict["text"])
-    #     self.assertEqual(self.user2, question.asker)
-    #     # make sure question and post are identified together
-    #     self.assertEqual(question.post, post)
-    #     # thread should have 2 post. test associations
-    #     self.assertEqual(thread.post_set.all().count(), 2)
-    #     self.assertEqual(thread, post.thread)
+    def test_question_no_text(self):
+        question_dict = {
+            "topic": "sometopic",
+            "tags": ["A", "B", "C"],
+        }
+        request = self.factory.post(
+            "/api/question/",
+            question_dict,
+        )
+        force_authenticate(request, self.asker_user)
+        response = views.question(request)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content["message"], "Your post needs text.")
 
-    # def test_question_no_thread_fails(self):
-    #     post_dict = {"text": "follow up post"}
-    #     request = self.factory.post(
-    #         "/api/post/",
-    #         post_dict,
-    #     )
-    #     request.META["HTTP_AUTHORIZATION"] = f"Bearer {self.token2}"
-    #     response = views.question(request)
-    #     content = json.loads(response.content)
-    #     self.assertNotIn("post", content.keys())
-    #     self.assertEqual(
-    #         content["message"],
-    #         "One of thread or topic must be specified. You cannot specify both.",
-    #     )
+    def test_question_both_thread_and_topic(self):
+        question_dict = {
+            "thread": 1,
+            "topic": "sometopic",
+            "text": "I am wondering what to do about this topic.",
+            "tags": ["A", "B", "C"],
+        }
+        request = self.factory.post(
+            "/api/question/",
+            question_dict,
+        )
+        force_authenticate(request, self.asker_user)
+        response = views.question(request)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            content["message"],
+            "One of thread or topic must be specified. You cannot specify both.",
+        )
 
-    # def test_thread_no_text_fails(self):
-    #     post_dict = {"topic": "topic"}
-    #     request = self.factory.post(
-    #         "/api/post/",
-    #         post_dict,
-    #     )
-    #     request.META["HTTP_AUTHORIZATION"] = f"Bearer {self.token2}"
-    #     response = views.question(request)
-    #     content = json.loads(response.content)
-    #     self.assertNotIn("post", content.keys())
-    #     self.assertEqual(content["message"], "Your post needs text.")
+    def test_question_invalid_contract(self):
+        question_dict = {
+            "topic": "sometopic",
+            "text": "I am wondering what to do about this topic.",
+            "tags": ["A", "B", "C"],
+            "questionAddress": "0xInvalidAddress",
+        }
+        request = self.factory.post(
+            "/api/question/",
+            question_dict,
+        )
+        force_authenticate(request, self.asker_user)
+        response = views.question(request)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content["message"], "Failed to load contract.")
 
-    # def test_reply_to_thread_no_text_fails(self):
-    #     # make thread
-    #     request = self.factory.post(
-    #         "/api/post/",
-    #         question_dict,
-    #     )
-    #     request.META["HTTP_AUTHORIZATION"] = f"Bearer {self.token}"
-    #     response = views.post(request)
-    #     content = json.loads(response.content)
-    #     self.assertEqual(response.status_code, 200)
-    #     thread = Thread.objects.get(pk=content["thread"])
-    #     # post again
-    #     post_dict = {"thread": thread.pk}
-    #     request = self.factory.post(
-    #         "/api/post/",
-    #         post_dict,
-    #     )
-    #     request.META["HTTP_AUTHORIZATION"] = f"Bearer {self.token2}"
-    #     response = views.question(request)
-    #     content = json.loads(response.content)
-    #     self.assertNotIn("post", content.keys())
-    #     self.assertEqual(content["message"], "Your post needs text.")
+    def test_question_invalid_owner(self):
+        question_dict = {
+            "topic": "sometopic",
+            "text": "I am wondering what to do about this topic.",
+            "tags": ["A", "B", "C"],
+        }
+        # deploy question with invalid owner
+        abi = question_contract["abi"]
+        bytecode = question_contract["bytecode"]["object"]
+        questionContract = w3.eth.contract(abi=abi, bytecode=bytecode)
+        questionHash = Web3.solidity_keccak(
+            ["address", "string"], [self.asker, question_dict["text"]]
+        )
+        tx_hash = questionContract.constructor(
+            self.oracle, self.oracle, self.asker, questionHash
+        ).transact({"from": self.asker, "value": 1})
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash, 180)
+        question_dict["questionAddress"] = tx_receipt["contractAddress"]
+        request = self.factory.post(
+            "/api/question/",
+            question_dict,
+        )
+        force_authenticate(request, self.asker_user)
+        response = views.question(request)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content["message"], "Invalid owner.")
+
+    def test_question_invalid_asker(self):
+        question_dict = {
+            "topic": "sometopic",
+            "text": "I am wondering what to do about this topic.",
+            "tags": ["A", "B", "C"],
+        }
+        # deploy question with invalid asker
+        abi = question_contract["abi"]
+        bytecode = question_contract["bytecode"]["object"]
+        questionContract = w3.eth.contract(abi=abi, bytecode=bytecode)
+        questionHash = Web3.solidity_keccak(
+            ["address", "string"], [self.oracle, question_dict["text"]]
+        )
+        tx_hash = questionContract.constructor(
+            self.owner, self.oracle, self.oracle, questionHash
+        ).transact({"from": self.oracle, "value": 1})
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash, 180)
+        question_dict["questionAddress"] = tx_receipt["contractAddress"]
+        request = self.factory.post(
+            "/api/question/",
+            question_dict,
+        )
+        force_authenticate(request, self.asker_user)
+        response = views.question(request)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content["message"], "Invalid asker.")
+
+
+class TestAnswers(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        #
+        self.eth_tester = provider.ethereum_tester
+        self.owner = self.eth_tester.get_accounts()[0]
+        self.oracle = self.eth_tester.get_accounts()[1]
+        self.asker = self.eth_tester.get_accounts()[2]
+        self.asker_user = User.objects.create_user_address(self.asker)
+        self.answerer = self.eth_tester.get_accounts()[3]
+        self.answerer_user = User.objects.create_user_address(self.answerer)
+        views.allowed_owners.append(self.owner)
